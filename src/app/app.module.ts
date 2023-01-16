204cada8d10b3640d8219870bb4fd721d253a3da
ShamseenaS <shamseena.s@finastra.com>
2023-01-16 15:11:16 +0530

    configuration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,13 +16,15 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { OnlyNumberDirective } from './only-number.directive';
 import {HttpClientModule } from '@angular/common/http';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ToolbarComponent,
-    OnlyNumberDirective
+    OnlyNumberDirective,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
